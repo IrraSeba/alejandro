c494190c3783276a35002b585552a3e176ad8b69
IrraSeba <IrraSeba@RA52PCALU-31510.informatica.st>
2017-05-24 11:24:05 -0300

    se crean metodos

---
+++ JavaApplication2/src/javaapplication2/si.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author IrraSeba
+ */
+public class si {
+    public int l,a,q;
+
+    public si() {
+    }
+
+    public si(int l, int a, int q) {
+        this.l = l;
+        this.a = a;
+        this.q = q;
+    }
+
+    public int getL() {
+        return l;
+    }
+
+    public void setL(int l) {
+        this.l = l;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getQ() {
+        return q;
+    }
+
+    public void setQ(int q) {
+        this.q = q;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
